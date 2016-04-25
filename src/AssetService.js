7e64c6b170468560f29eb43d9545a161444c9db2
Guillaume Lesniak <guillaume@netlor.fr>
2016-04-25 10:38:50 +0200

    KB Mgmt: Assets: Add CRUD frontend

---
+++ src/AssetService.js
@@ -0,0 +1,73 @@
+(function () {
+
+    angular
+        .module('BackofficeApp')
+        .factory('AssetService', [ '$resource', '$http', '$q', '$httpParamSerializer',
+            AssetService
+        ]);
+
+    function AssetService($resource, $http, $q, $httpParamSerializer) {
+        var self = this;
+
+        self.AssetResource = $resource('/api/assets/:assetId', { assetId: '@id' });
+
+        var getAssets = function (params) {
+            var promise = $q.defer();
+            var q = $httpParamSerializer(params);
+
+            $http.get('/api/assets?' + q).then(
+                function (data) { promise.resolve(data.data);  },
+                function (data) { promise.reject(data.status); }
+            );
+
+            return promise.promise;
+        };
+
+        var createAsset = function (params) {
+            var promise = $q.defer();
+
+            var asset = new self.AssetResource(params);
+            asset.$save(function (asset) {
+                promise.resolve(asset);
+            }, function (error) {
+                promise.reject(error.status);
+            });
+
+            return promise.promise;
+        };
+
+        var updateAsset = function (params) {
+            var promise = $q.defer();
+
+            var asset = new self.AssetResource(params);
+            asset.$save(function (asset) {
+                promise.resolve(asset);
+            }, function (error) {
+                promise.reject(error.status);
+            });
+
+            return promise.promise;
+        };
+
+        var deleteAsset = function (id) {
+            var promise = $q.defer();
+
+            self.AssetResource.delete({assetId: id}, function () {
+                promise.resolve();
+            }, function (error) {
+                promise.reject(error.status);
+            });
+
+            return promise.promise;
+        };
+
+        return {
+            getAssets: getAssets,
+            createAsset: createAsset,
+            deleteAsset: deleteAsset,
+            updateAsset: updateAsset
+        };
+    }
+
+})
+();
--- src/BackofficeKbInfoCtrl.js
+++ src/BackofficeKbInfoCtrl.js
@@ -3,14 +3,16 @@
     angular
         .module('BackofficeApp')
         .controller('BackofficeKbInfoCtrl', [
-            '$scope', '$mdToast', 'gettextCatalog', 'KBService', 'TableHelperService', 'ErrorService',
+            '$scope', '$mdToast', '$mdMedia', '$mdDialog', 'gettext', 'gettextCatalog', 'KBService',
+            'TableHelperService', 'ErrorService', 'AssetService',
             BackofficeKbInfoCtrl
         ]);
 
     /**
      * BO > KB > INFO
      */
-    function BackofficeKbInfoCtrl($scope, $mdToast, gettextCatalog, KBService, TableHelperService, ErrorService) {
+    function BackofficeKbInfoCtrl($scope, $mdToast, $mdMedia, $mdDialog, gettext, gettextCatalog, KBService,
+                                  TableHelperService, ErrorService, AssetService) {
         TableHelperService.resetBookmarks();
 
         /*
@@ -36,6 +38,122 @@
 
         TableHelperService.watchSearch($scope, 'assets.query.filter', $scope.assets.query, $scope.updateAssets);
         $scope.updateAssets();
+
+
+        $scope.createNewAsset = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', CreateAssetDialogCtrl],
+                templateUrl: '/views/dialogs/create.assets.html',
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (asset) {
+                    AssetService.createAsset(asset).then(
+                        function () {
+                            $scope.updateAssets();
+                            $mdToast.show(
+                                $mdToast.simple()
+                                    .textContent(gettext('The asset has been created successfully.'))
+                                    .position('top right')
+                                    .hideDelay(3000)
+                            );
+                        },
+
+                        function (status) {
+                            ErrorService.notifyFetchError(gettext('created asset'), status);
+                        }
+                    );
+                }, function () {
+
+                });
+        };
+
+        $scope.editAsset = function (ev, asset) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', 'user', CreateAssetDialogCtrl],
+                templateUrl: '/views/dialogs/create.users.admin.html',
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen,
+                locals: {
+                    'asset': asset
+                }
+            })
+                .then(function (asset) {
+                    AssetService.updateAsset(asset).then(
+                        function () {
+                            $scope.updateAssets();
+                            $mdToast.show(
+                                $mdToast.simple()
+                                    .textContent(gettext('The asset has been updated successfully.'))
+                                    .position('top right')
+                                    .hideDelay(3000)
+                            );
+                        },
+
+                        function (status) {
+                            ErrorService.notifyFetchError(gettext('updated asset'), status);
+                        }
+                    );
+                }, function () {
+
+                });
+        };
+
+        $scope.deleteAsset = function (ev, item) {
+            var confirm = $mdDialog.confirm()
+                .title(gettextCatalog.getString('Are you sure you want to delete asset "{{ label }}"?',
+                    {label: item.label}))
+                .textContent(gettext('This operation is irreversible.'))
+                .targetEvent(ev)
+                .ok(gettext('Delete'))
+                .cancel(gettext('Cancel'));
+            $mdDialog.show(confirm).then(function() {
+                AssetService.deleteAsset(item.id).then(
+                    function () {
+                        $scope.updateAssets();
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent(gettextCatalog.getString('The asset "{{label}}" has been deleted.',
+                                    {label: item.label}))
+                                .position('top right')
+                                .hideDelay(3000)
+                        );
+                    },
+
+                    function (status) {
+                        ErrorService.notifyFetchError(gettext('deleted asset'), status);
+                    }
+                );
+            }, function() {
+            });
+        };
     }
 
+    function CreateAssetDialogCtrl($scope, $mdDialog, asset) {
+        if (asset != undefined && asset != null) {
+            $scope.asset = asset;
+        } else {
+            $scope.asset = {
+                mode: 1,
+                code: '',
+                type: 1,
+                label: '',
+                description: ''
+            };
+        }
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+
+        $scope.create = function() {
+            $mdDialog.hide($scope.asset);
+        };
+    }
 })();
