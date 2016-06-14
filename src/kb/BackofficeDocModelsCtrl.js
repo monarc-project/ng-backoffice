e1de3b17051695ccd1484f43ab73cb0f29c6c706
Guillaume Lesniak <guillaume@netlor.fr>
2016-06-14 10:17:02 +0200

    DocModels: Upload and send fields in the same POST request

--- src/kb/BackofficeDocModelsCtrl.js
+++ src/kb/BackofficeDocModelsCtrl.js
@@ -27,23 +27,19 @@
             var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
 
             $mdDialog.show({
-                controller: ['$scope', '$mdDialog', 'DocModelService', 'Upload', CreateDocModelsDialogCtrl],
+                controller: ['$scope', '$mdDialog', '$mdToast', 'DocModelService', 'Upload', CreateDocModelsDialogCtrl],
                 templateUrl: '/views/dialogs/create.docmodels.html',
                 targetEvent: ev,
                 clickOutsideToClose: true,
                 fullscreen: useFullScreen
             })
                 .then(function (docModel) {
-                    DocModelService.createDocModel(docModel,
-                        function () {
-                            $scope.updateDocModels();
-                            $mdToast.show(
-                                $mdToast.simple()
-                                    .textContent(gettext('The docModel has been created successfully.'))
-                                    .position('top right')
-                                    .hideDelay(3000)
-                            );
-                        }
+                    $scope.updateDocModels();
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .textContent(gettext('The document has been created successfully.'))
+                            .position('top right')
+                            .hideDelay(3000)
                     );
                 });
         };
@@ -51,52 +47,55 @@
     }
 
 
-    function CreateDocModelsDialogCtrl($scope, $mdDialog, DocModelService, Upload) {
+    function CreateDocModelsDialogCtrl($scope, $mdDialog, $mdToast, DocModelService, Upload) {
         $scope.docModel = {
             category: null,
-            description: '',
-            file: null
+            description: ''
         };
 
         $scope.uploadProgress = null;
         $scope.categories = DocModelService.getCategories()
 
         // Upload system
-        $scope.$watch('file', function () {
-            if ($scope.file != null) {
-                $scope.upload($scope.file);
-            }
-        })
-
-        $scope.upload = function (file) {
-            if (file) {
-                if (!file.$error) {
-                    $scope.uploadProgress = 0;
-
-                    Upload.upload({
-                        url: '/api/models/upload',
-                        file: file
-                    }).then(function (resp) {
-                            $scope.uploadProgress = null;
-                            $scope.docModel.file = resp.data.id;
-                        }, function (resp) {
-
-                        }
-                        , function (evt) {
-                            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                            $scope.uploadProgress = progressPercentage;
-                        })
-                }
-            }
-        }
-
+        $scope.$watch('file', function (newValue, oldValue) {
+            console.log(newValue);
+        });
 
         $scope.cancel = function () {
             $mdDialog.cancel();
         };
 
         $scope.create = function () {
-            $mdDialog.hide($scope.docModel);
+            if (!$scope.file.$error) {
+                $scope.uploadProgress = 0;
+
+                Upload.upload({
+                    url: '/api/docmodels',
+                    file: $scope.file,
+                    data: {category: $scope.docModel.category, description: $scope.docModel.description}
+                }).then(function (resp) {
+                        $scope.uploadProgress = null;
+                        $mdDialog.hide($scope.docModel);
+                    }, function (resp) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent(gettext('Error while uploading: ' + resp.status))
+                                .position('top right')
+                                .hideDelay(3000)
+                        );
+                    }
+                    , function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        $scope.uploadProgress = progressPercentage;
+                    })
+            } else {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent(gettext('File error: ' + $scope.file.$error))
+                        .position('top right')
+                        .hideDelay(3000)
+                );
+            }
         };
     }
 
