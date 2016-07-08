d99891fd7c0354463ad8f0ead5e63a16a1da5120
Guillaume Lesniak <guillaume@netlor.fr>
2016-07-08 10:47:08 +0200

    DocModels: Add edit button

--- src/kb/BackofficeDocModelsCtrl.js
+++ src/kb/BackofficeDocModelsCtrl.js
@@ -39,6 +39,25 @@
                 });
         };
 
+        $scope.editDocmodel = function (ev, docmodel) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', 'toastr', 'gettext', 'DocModelService', 'Upload', 'docmodel', CreateDocModelsDialogCtrl],
+                templateUrl: '/views/dialogs/create.docmodels.html',
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen,
+                locals: {
+                    docmodel: docmodel
+                }
+            })
+                .then(function (docModel) {
+                    $scope.updateDocModels();
+                    toastr.success(gettext('The document has been updated successfully.'), gettext('Update successful'));
+                });
+        };
+
         $scope.deleteDocmodel = function (ev, item) {
             var confirm = $mdDialog.confirm()
                 .title(gettextCatalog.getString('Are you sure you want to delete the document "{{ label }}"?',
@@ -65,11 +84,15 @@
     }
 
 
-    function CreateDocModelsDialogCtrl($scope, $mdDialog, toastr, gettext, DocModelService, Upload) {
-        $scope.docModel = {
-            category: null,
-            description: ''
-        };
+    function CreateDocModelsDialogCtrl($scope, $mdDialog, toastr, gettext, DocModelService, Upload, docmodel) {
+        if (docmodel) {
+            $scope.docModel = docmodel;
+        } else {
+            $scope.docModel = {
+                category: null,
+                description: ''
+            };
+        }
 
         $scope.uploadProgress = null;
         $scope.categories = DocModelService.getCategories()
@@ -84,7 +107,8 @@
                 $scope.uploadProgress = 0;
 
                 Upload.upload({
-                    url: '/api/docmodels',
+                    url: $scope.docModel.id ? '/api/docmodels/' + $scope.docModel.id : '/api/docmodels',
+                    method: $scope.docModel.id ? 'PUT' : 'POST',
                     file: $scope.file,
                     data: {category: $scope.docModel.category, description: $scope.docModel.description}
                 }).then(function (resp) {
@@ -101,6 +125,12 @@
                     })
             } else if ($scope.file && $scope.file.$error) {
                 toastr.error($scope.file.$error, gettext('File error'));
+            } else if ($scope.docModel.id > 0) {
+                DocModelService.updateDocModel($scope.docModel, function () {
+                    $mdDialog.hide($scope.docModel);
+                }, function () {
+
+                });
             } else {
                 toastr.warning(gettext('You must select a file'));
             }
