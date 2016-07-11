999441f913a236eafdf5bb09b06c230c1deb2405
Guillaume Lesniak <guillaume@netlor.fr>
2016-07-11 10:28:04 +0200

    Add early risk sheet layout

--- src/kb/models/BackofficeAnrObjectInstanceCtrl.js
+++ src/kb/models/BackofficeAnrObjectInstanceCtrl.js
@@ -13,6 +13,13 @@
      */
     function BackofficeAnrObjectInstanceCtrl($scope, toastr, $mdMedia, $mdDialog, gettext, gettextCatalog,
                                            TableHelperService, ModelService, ObjlibService, $stateParams) {
+        $scope.openRiskSheet = function () {
+            $scope.sheet_id = 1;
+        };
+
+        $scope.resetSheet = function () {
+            $scope.sheet_id = undefined;
+        }
     }
 
 })();
