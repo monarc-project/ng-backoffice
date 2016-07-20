7123252b908f5ede25741f6e4ff13ccc31968c0c
Guillaume Lesniak <guillaume@netlor.fr>
2016-07-20 11:45:56 +0200

    Plug the password change fields into the API

--- src/BackofficeAccountCtrl.js
+++ src/BackofficeAccountCtrl.js
@@ -3,15 +3,29 @@
     angular
         .module('BackofficeApp')
         .controller('BackofficeAccountCtrl', [
-            '$scope', 'gettextCatalog',
+            '$scope', 'gettext', 'gettextCatalog', 'toastr', '$http', 'UserService',
             BackofficeAccountCtrl
         ]);
 
     /**
      * Account Controller for the Backoffice module
      */
-    function BackofficeAccountCtrl($scope, gettextCatalog) {
+    function BackofficeAccountCtrl($scope, gettext, gettextCatalog, toastr, $http, UserService) {
         $scope.language = 'en';
+
+        $scope.password = {
+            old: '',
+            new: '',
+            confirm: ''
+        }
+
+        $scope.updatePassword = function () {
+            $http.put('/api/user/password/' + UserService.getUserId(), $scope.password).then(function (data) {
+                if (data.data.status == 'ok') {
+                    toastr.success(gettext('Your password has been updated successfully'));
+                }
+            })
+        }
 
         $scope.onLanguageChanged = function () {
             gettextCatalog.setCurrentLanguage($scope.language);
