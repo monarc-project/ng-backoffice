7a01236f29ecd15160393ad3bf846b0dc3301c06
Thomas Metois <thomas@netlor.fr>
2016-09-13 10:47:50 +0200

    Erreur synthaxe fct

--- src/DownloadService.js
+++ src/DownloadService.js
@@ -9,13 +9,19 @@
     function DownloadService() {
         var self = this;
 
-        var downloadBlob = function (data, name, typeF = 'octet/stream') {
+        var downloadBlob = function (data, name, typeF) {
+            if(typeF == undefined){
+                typeF = 'octet/stream';
+            }
             var saveData = (function () {
                 var a = document.createElement('a');
                 document.body.appendChild(a);
                 a.style = 'display: none';
 
-                return function (blobData, fileName,typeF = 'octet/stream') {
+                return function (blobData, fileName,typeF) {
+                    if(typeF == undefined){
+                        typeF = 'octet/stream';
+                    }
                     var blob = new Blob([blobData], {type: typeF}),
                         url = window.URL.createObjectURL(blob);
 
