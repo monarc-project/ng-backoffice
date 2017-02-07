50fc164bc153560e38e0b65136eed71589ec8ccf
Thomas Metois <thomas@netlor.fr>
2017-02-07 16:42:16 +0100

    Fix Model load

---
+++ src/ClientRecommandationService.js
@@ -0,0 +1,11 @@
+(function () {
+
+    angular
+        .module('BackofficeApp')
+        .factory('ClientRecommandationService', [ ClientRecommandationService ]);
+
+	function ClientRecommandationService(){
+		return {};
+	}
+})
+();
