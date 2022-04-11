eb155175d936222407c65652c1f409c2d5b8a8fb
jfrocha <juanferocha@hotmail.com>
2022-04-11 10:50:26 +0200

    Removed useless code

--- src/kb/models/BackofficeKbModelsCtrl.js
+++ src/kb/models/BackofficeKbModelsCtrl.js
@@ -181,18 +181,12 @@
             }
         })
 
-
-        $scope.contextFieldsLanguage = [];
         $scope.contextFields = {
             1:[],
             2:[],
             3:[],
             4:[]
         }
-
-        $scope.addAssetContextField = function(index, chip) {
-            console.log($scope.contextFields);
-        };
 
         $scope.cancel = function() {
             $mdDialog.cancel();
