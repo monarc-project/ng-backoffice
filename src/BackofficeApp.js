785f82880afb5b72a1ddc0d57500bf6a6c347531
Ruslan Baidan <ruslan.baidan@nc3.lu>
2024-09-13 15:50:05 +0200

    Added one more condition to prevent the redirect to the login page when password reset is opened.

--- src/BackofficeApp.js
+++ src/BackofficeApp.js
@@ -220,7 +220,7 @@ angular
 
                         if (response.status == 401) {
                             const state = $injector.get('$state');
-                            if (state.current.name !== 'passwordforgotten') {
+                            if (state.current.name !== 'passwordforgotten' && state.current.name !== '') {
                                 state.transitionTo('login');
                             }
                         } else if (response.status == 403) {
